5ef386315df2ffc3f59915f842b930220f8265dd
Sabrina <saandradepereira@gmail.com>
2017-10-02 20:37:04 -0300

    Segundo commit

---
+++ fib-calc-api/src/main/java/br/edu/univas/FibonacciCalcService.java
@@ -0,0 +1,17 @@
+package br.edu.univas;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/fibcalc")
+public interface FibonacciCalcService {
+	
+	@GET
+	@Path("/fib/{num}")
+	@Produces(MediaType.APPLICATION_JSON)
+	Result fibcalc(@PathParam("num") int num);
+}
--- fib-calc-api/src/main/java/br/edu/univas/Result.java
+++ fib-calc-api/src/main/java/br/edu/univas/Result.java
@@ -1,5 +1,29 @@
 package br.edu.univas;
 
 public class Result {
+	
+		private int num;
+		private int fib;
 
-}
+		public Result(int num, int fib) {
+			this.num = num;
+			this.fib = fib;
+		}
+		
+		public int getNum() {
+			return num;
+		}
+
+		public void setNum(int num) {
+			this.num = num;
+		}
+
+		public int getFib() {
+			return fib;
+		}
+
+		public void setFib(int fib) {
+			this.fib = fib;
+		}
+	}
+	
